907bb7e2a568013352a08dd572017cc36eb7cb2c
Pranita Aware <94918654+aware-96-K@users.noreply.github.com>
2023-03-02 17:31:45 +0530

    Add files via upload

---
+++ Practicals/TestAbstraction.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class TestAbstraction
+{
+
+	public static void main(String[] args) 
+	{
+		AbstractionData ae=new AbstractionData(1,"Pranita","mumbai",40000.00);
+		//call method
+		ae.mailCheck();
+		
+	}
+
+}
