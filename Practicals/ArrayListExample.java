816b755bc8094300729538a74e9eaa7c30aea81d
Pranita Aware <94918654+aware-96-K@users.noreply.github.com>
2023-04-06 18:04:35 +0530

    Add files via upload

---
+++ Practicals/ArrayListExample.java
@@ -0,0 +1,23 @@
+package Collection;
+import java.util.*;//import java utility class
+
+public class ArrayListExample 
+{
+
+	public static void main(String[] args) 
+	{
+		ArrayList<String> arraylist=new ArrayList<String>();
+		//adding object in Arraylist
+		arraylist.add("mango");
+		arraylist.add("Apple");
+		arraylist.add("orange");
+		arraylist.add("Grapes");
+		arraylist.add("mango");
+		//traversing list to iterator
+		Iterator itr=arraylist.iterator();
+		while(itr.hasNext())
+		{
+		System.out.println(itr.next());	
+		}
+	}
+}
