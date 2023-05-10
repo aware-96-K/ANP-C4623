60fc3b429274334526fd42e0bff0c9633c9d803d
Pranita Aware <94918654+aware-96-K@users.noreply.github.com>
2023-05-10 14:42:40 +0530

    Add files via upload

--- Project/App.java
+++ Project/App.java
@@ -1,35 +1,24 @@
-package com.HotelManagementSystem.HotelManagement;
+package com.management.main;
 
-import java.util.List;
+import com.management.dao.BloodDao;
+import com.management.model.Blood;
 
-import com.maven.dao.Hotel;
-import com.maven.model.HotelManagement;
-
-
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-    	Hotel hm = new Hotel();
 
-    	HotelManagement customer =new HotelManagement("Pranita", "Aware", "pranitaaware13@gmail.com", 89, "Royal", 789, "Normal", 56789);
-    	HotelManagement customer1 =new HotelManagement("Mahendra", "Dhoni", "mahi7@gmail.com", 79, "Normal", 489, "Golden", 9876654);
-    	HotelManagement customer2 =new HotelManagement("Akshay", "Kumar", "akshaykumar@gmail.com", 98, "Royal", 489, "Golden", 778654);
-    	HotelManagement customer3 =new HotelManagement("Allu", "Arjun", "alluarjun@gmail.com", 77, "Royal", 489, "Normal", 778654);
+	public static void main(String[] args) 
+	{
+		BloodDao dao = new BloodDao();	    
+	    // Create a new user entity
+	    Blood blood = new Blood();
+	    blood.setName("John Smith");
+	    blood.setBloodGroup("B+");
+	   dao.saveBlood(blood);
+	    
+	    blood.setName("abc xyz");
+	    blood.setBloodGroup("AB+");
+	    dao.updateUser(blood);
+	    dao.deleteBlood(blood);
+	}
 
-        hm.saveCustomer(customer);
-        hm.saveCustomer(customer1); 
-        hm.saveCustomer(customer2);
-        
-        customer.setFirstName("pva");
-        hm.updateCustomer(customer);
-        
-
-        List <HotelManagement > customers = hm.getAllCustomers();
-        customers.forEach(Customer2 -> System.out.println(Customer2.getId()));
-        
-        hm.deleteCustomer(2);
-
-    }
-    }
-
+}
